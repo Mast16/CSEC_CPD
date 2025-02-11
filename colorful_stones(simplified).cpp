a40fe56af6974df348b313b9f65cdb49c27b543e
Mast16 <mastewalgshiferaw@gmail.com>
2025-02-12 00:35:38 +0300

    Create colorful_stones(simplified).cpp

---
+++ colorful_stones(simplified).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    int position = 0; 
+
+    for (int i = 0; i < t.length(); i++) {
+        
+        if (s[position] == t[i]) {
+            position++;
+        }
+    }
+
+    
+    cout << position + 1 << endl;
+
+    return 0;
+}
